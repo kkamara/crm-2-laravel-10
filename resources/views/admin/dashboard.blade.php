e835a701c5914c3ab76991c95d712cb0fd07adc9
kkamara <kamaracomputers@gmail.com>
2023-08-19 18:30:29 +0100

    Add admin dashboard view

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Dashboard | {{ config('app.name') }}</title>
+    <link rel='icon' type='image/ico' href="{{ asset('favicon.ico') }}" />
+    <link rel='icon' href="{{ asset('css/admin/theme.css') }}" />
+</head>
+<body>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('admin', function () {
     return view('admin/dashboard');
 });
