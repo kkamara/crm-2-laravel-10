5526024e6e32703c2ecaee51e49b70c87455859e
kkamara <kamaracomputers@gmail.com>
2023-08-20 16:36:43 +0100

    Add card header for admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -101,6 +101,18 @@
           <div class="row">
             <div class="col-lg-12 d-flex align-items-strech">
               <div class="card w-100">
+                <div class="card-header">
+                    <h3>
+                        Dashboard
+                        <div class="float-end">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                            <li class="breadcrumb-item active" aria-current="page">Home</li>
+                            </ol>
+                        </nav>
+                        </div>
+                    </h3>
+                </div>
                 <div class="card-body">
                   <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
                     Show different graphs & statistics here based on permissions.
