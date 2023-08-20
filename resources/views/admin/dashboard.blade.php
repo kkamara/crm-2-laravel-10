e4f55ebe322fc40cd66543b24248dc3830c14a56
kkamara <kamaracomputers@gmail.com>
2023-08-20 16:29:14 +0100

    Increase admin dashboard page width

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -99,7 +99,7 @@
         <div class="container-fluid">
           <!--  Row 1 -->
           <div class="row">
-            <div class="col-lg-8 d-flex align-items-strech">
+            <div class="col-lg-12 d-flex align-items-strech">
               <div class="card w-100">
                 <div class="card-body">
                   <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
