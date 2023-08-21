1064a5b6ab52f4e2dcb003051e9b29ecb6ffdefd
kkamara <kamaracomputers@gmail.com>
2023-08-21 10:18:37 +0100

    Define roles & permissions for all access user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use \App\Models\User;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,15 +15,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $role = Role::create(['name' => 'all access']);
+        $permissions = json_decode(
+            file_get_contents(__DIR__.'/permissions.json'),
+            false
+        );
+        foreach($permissions->data as $key => $permission)
+        {
+            $p = Permission::create(['name' => $permission]);
+            $role->givePermissionTo($p);
+        }
+        
+
         $email = 'admin@example.com';
+        $user = User::where(
+            'email', $email
+        )->first();
         if (
-            null !== User::where(
-                'email', $email
-            )->first()
+            null !== $user
         ) {
+            echo 'Not creating a user because user with admin@example.com already exists'.
+                PHP_EOL;
+            $user->assignRole('all access');
             return;
         }
-        User::factory()->count(1)
+        echo 'Creating user with email admin@example.com'.PHP_EOL;
+        $user = User::factory()->count(1)
             ->create(compact('email'));
+        $user[0]->assignRole('all access');
     }
 }
