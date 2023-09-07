3ff44aa12d5dde341cf17696158e51bc95339f98
kkamara <kamaracomputers@gmail.com>
2023-09-07 10:23:58 +0100

    Add soft deletes to Client resource

--- app/Models/Admin/Client.php
+++ app/Models/Admin/Client.php
@@ -5,10 +5,11 @@ namespace App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'users_id',
